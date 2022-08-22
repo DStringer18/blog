7ad207fd25030ab695e9de67bc2b77fc2cb76960
dylan <dylan.stringer@timepress.co.uk>
2022-08-22 15:26:12 +0200

    update name of app

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Wander_In_My_Time
+module WonderLust
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
