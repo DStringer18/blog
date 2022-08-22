0436cb1fc34515c3d673758fbbd9b7979bfef756
dylan <dylan.stringer@timepress.co.uk>
2022-08-22 15:24:25 +0200

    adjust indexes

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,2 +1,10 @@
 class Tour < ApplicationRecord
+  include Archivable
+
+  has_one_attached :image, dependent: :destroy do |attachable|
+    attachable.variant :header, resize_to_limit: [400, 400]
+    attachable.variant :thumb, resize_to_limit: [400, 400]
+  end
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
 end
